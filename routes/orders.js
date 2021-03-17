2208656f65fe4f98fd46e3b7e7de216ace0e4f91
Gentjan Ferati <gentjan.ferati@fshnstudent.info>
2021-03-17 21:16:48 +0100

    first commit

---
+++ routes/orders.js
@@ -0,0 +1,131 @@
+const {
+    Order
+} = require('../models/order');
+const express = require('express');
+const {
+    OrderItem
+} = require('../models/order-item');
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+    const orderList = await Order.find().populate('user', 'name');
+
+    if (!orderList) {
+        res.status(500).json({
+            success: false
+        })
+    }
+    res.send(orderList);
+})
+
+router.get(`/:id`, async (req, res) => {
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems',
+            populate: {
+                path: 'product',
+                populate: 'category'
+            }
+        });
+
+    if (!order) {
+        res.status(500).json({
+            success: false
+        })
+    }
+    res.send(order);
+})
+
+router.post('/', async (req, res) => {
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem => {
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+        newOrderItem = await newOrderItem.save();
+        return newOrderItem._id;
+    }))
+    const orderItemsIdsResolved = await orderItemsIds;
+
+const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId)=>{
+    const orderItem = await OrderItem.findById(orderItemId).populate('product','price');
+    const totalPrice = orderItem.product.price *  orderItem.quantity;
+    return totalPrice
+})) 
+const totalPrice = totalPrices.reduce((a,b)=> a+b, 0);
+
+
+console.log(totalPrices)
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,   
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: totalPrice,
+        user: req.body.user
+
+
+    })
+    order = await order.save();
+    if (!order)
+        return res.status.apply(404).send('the order canot be created');
+    res.send(order);
+})
+
+router.put('/:id', async (req, res)=>{
+    const order = await Order.findByIdAndUpdate(
+        req.params.id, 
+        {
+            status: req.body.status
+        },
+        {new: true}
+    )
+    if(!order){
+        res.status(500).json({message: 'The order with the given ID is not found'})
+    }
+    res.status(200).send(order);
+})
+router.delete('/:id', (req, res)=>{
+    Order.findByIdAndRemove(req.params.id).then(async order => {
+        if(order){
+            await order.orderItems.map( async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+            
+        return res.status(200).json({success: true, message: 'the order is deleted'})
+        }
+        else {
+            return res.status(404).json({success: false, message: 'order not found'})
+        }
+    }).catch(err=> {
+        return res.status(400).json({success: false, error: err})
+    })
+}) 
+router.get('/get/totalsales', async (req, res)=>{
+    const totalSales = await Order.aggregate([
+        {  $group: {_id: null, totalsales: {$sum: '$totalPrice'}}}
+    ])
+    if(!totalSales){
+        return res.status(400).send('The order sales cannot be generated')
+    }
+    res.send({totalsales: totalSales.pop().totalsales })
+})
+
+router.get(`/get/count`, async (req, res) =>{
+    const orderCount = await Order.countDocuments((count)=> count);
+
+    if(!orderCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        count: orderCount
+    });
+})
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,63 @@
+const {User} = require('../models/user');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+router.get(`/`, async (req, res) =>{
+    const userList = await User.find().select('-passwordHash');
+
+    if(!userList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(userList);
+})
+router.get(`/:id`, async (req, res) =>{
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if(!user) {
+        res.status(500).json({message: "the user with the given id is not found"})
+    } 
+    res.status(200).send(user);
+})
+router.post('/', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        color: req.body.color,
+        street: req.body.street,
+        passwordHash: bcrypt.hashSync(req.body.password, 10), 
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+
+    })
+    user = await user.save();
+    if(!user)
+    return res.status.apply(404).send('the category canot be created');
+    res.send(user);
+})
+ router.post('/login', async (req ,res)=>{
+     const user = await User.findOne({email: req.body.email})
+     if(!user){
+        return res.status(400).send("The user not found");
+    }
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+        const token = jwt.sign(
+        {
+            userId: user.id
+        },
+        "secret"
+        )
+        res.status(200).send({user: user.email , token: token})
+    }else {
+        return res.status(400).send('password is worng');
+    }
+    
+    })
+
+
+
+module.exports =router;
